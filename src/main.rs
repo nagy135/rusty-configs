50a014eb5cca027a5b934292b301ede2337b3ed8
Viktor Nagy <viktor.nagy1995@gmail.com>
2020-11-30 21:59:05 +0100

    feat: working sqlite crate (rusqlite)

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,45 @@
-use rusqlite::{ Connection, NO_PARAMS };
-fn main() {
-    let conn = Connection::open("myfile.db").unwrap();
-    conn.execute("CREATE TABLE IF NOT EXISTS person (
-            id INTEGER PRIMARY KEY,
-            name TEXT NOT NULL,
-            email TEXT NOT NULL
-    )", NO_PARAMS).unwrap();
-    let name: String = "Steve Example".to_string();
-    let email: String = "steve@example.org".to_string();
-    conn.execute("INSERT INTO person (name, email) VALUES (?1, ?2)",
-    &[&name, &email]).unwrap();
+use rusqlite::{params, Connection, Result, NO_PARAMS};
+
+#[derive(Debug)]
+struct Person {
+    id: i32,
+    name: String,
+    data: Option<Vec<u8>>,
+}
+
+fn main() -> Result<()> {
+    // let conn = Connection::open("myfile.db").unwrap();
+    let conn = Connection::open_in_memory()?;
+
+    conn.execute(
+        "CREATE TABLE IF NOT EXISTS person (
+                  id              INTEGER PRIMARY KEY,
+                  name            TEXT NOT NULL,
+                  data            BLOB
+                  )",
+        NO_PARAMS,
+    )?;
+    let me = Person {
+        id: 0,
+        name: "Viktor".to_string(),
+        data: None,
+    };
+    conn.execute(
+        "INSERT INTO person (name, data) VALUES (?1, ?2)",
+        params![me.name, me.data],
+    )?;
+
+    let mut stmt = conn.prepare("SELECT id, name, data FROM person")?;
+    let person_iter = stmt.query_map(NO_PARAMS, |row| {
+        Ok(Person {
+            id: row.get(0)?,
+            name: row.get(1)?,
+            data: row.get(2)?,
+        })
+    })?;
+
+    for person in person_iter {
+        println!("Found person {:?}", person.unwrap());
+    }
+    Ok(())
 }
