ba84506244622edd72cf67314e25bd2ae5b6b6cd
Viktor Nagy <viktor.nagy1995@gmail.com>
2020-12-01 10:18:02 +0100

    feat: start of Entity trait

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 use rusqlite::{params, Connection, Result, NO_PARAMS};
 
 mod entities;
-use entities::{Config, Version};
+use entities::{Config, Version, Entity};
 
 fn main() -> Result<()> {
     Ok(())
@@ -42,5 +42,38 @@ fn db_test_config() -> Result<()> {
     assert_eq!(1, fetched_config.id);
     assert_eq!("/tmp/test", fetched_config.path);
     assert_eq!(vec!["first line".to_string(), "second line".to_string()], fetched_config.data);
+    Ok(())
+}
+
+#[test]
+fn db_test_version() -> Result<()> {
+    // let conn = Connection::open("myfile.db").unwrap();
+    let conn = Connection::open_in_memory()?;
+
+    conn.execute(
+        &Version::create_table(),
+        NO_PARAMS
+    )?;
+
+    let test_version = Version {
+        id: 1,
+        name: "home".to_string(),
+    };
+    conn.execute(
+        "INSERT INTO versions (name) VALUES (?1)",
+        params![test_version.name],
+    )?;
+
+    let mut stmt = conn.prepare("SELECT id, name FROM versions")?;
+    let mut versions = stmt.query_map(NO_PARAMS, |row| {
+        Ok(Version {
+            id: row.get(0)?,
+            name: row.get(1)?
+        })
+    })?;
+
+    let fetched_version: Version = versions.next().unwrap()?;
+    assert_eq!(1, fetched_version.id);
+    assert_eq!("home", fetched_version.name);
     Ok(())
 }
