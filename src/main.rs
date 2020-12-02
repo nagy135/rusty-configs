e29cf5f9f2a27b899fe6555fa64d16df2057ec5c
Viktor Nagy <viktor.nagy1995@gmail.com>
2020-12-02 20:04:21 +0100

    refactor: cleanup comments

--- src/main.rs
+++ src/main.rs
@@ -47,42 +47,6 @@ fn config_entity() -> Result<()> {
     // assert_eq!(vec!["first line".to_string(), "second line".to_string()], fetched_config.data);
 }
 
-// #[test]
-// fn db_test_config() -> Result<()> {
-//     // let db = Connection::open("myfile.db").unwrap();
-//     let db = Connection::open_in_memory()?;
-
-//     db.execute(
-//         &Config::table(),
-//         NO_PARAMS
-//     )?;
-
-//     let test_config = Config {
-//         id: 1,
-//         path: "/tmp/test".to_string(),
-//         data: vec!["first line".to_string(), "second line".to_string()],
-//     };
-//     db.execute(
-//         "INSERT INTO configs (path, data) VALUES (?1, ?2)",
-//         params![test_config.path, test_config.data.join("\n")],
-//     )?;
-
-//     let mut stmt = db.prepare("SELECT id, path, data FROM configs")?;
-//     let mut configs = stmt.query_map(NO_PARAMS, |row| {
-//         let data: String = row.get(2)?;
-//         Ok(Config {
-//             id: row.get(0)?,
-//             path: row.get(1)?,
-//             data: data.split('\n').into_iter().map(|x| x.to_string()).collect()
-//         })
-//     })?;
-
-//     let fetched_config: Config = configs.next().unwrap()?;
-//     assert_eq!(1, fetched_config.id);
-//     assert_eq!("/tmp/test", fetched_config.path);
-//     assert_eq!(vec!["first line".to_string(), "second line".to_string()], fetched_config.data);
-//     Ok(())
-// }
 
 // #[test]
 // fn db_test_version() -> Result<()> {
