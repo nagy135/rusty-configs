fe1c4af5cde2d0a185058920934e6f4f569ba31b
Viktor Nagy <viktor.nagy1995@gmail.com>
2020-12-09 19:48:43 +0100

    feat: testing script

---
+++ testing.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+commands="cargo build ; ./target/debug/rusty-configs init
+cargo build ; ./target/debug/rusty-configs read
+cargo build ; ./target/debug/rusty-configs write
+cargo build ; ./target/debug/rusty-configs delete -p /tmp/test
+cargo build ; ./target/debug/rusty-configs delete -i 1
+cargo build ; ./target/debug/rusty-configs delete -n test
+cargo build ; ./target/debug/rusty-configs add -p /tmp/test4 -g 1
+"
+cmd=$(echo "$commands" | fzf)
+[[ ! -z $cmd ]] && echo -e "=========\nrunning command:\n$cmd\n=========" && bash -c "$cmd"
