d5f148e198b3702aae465cd4650e4dc714ce2f97
Viktor Nagy <viktor.nagy1995@gmail.com>
2020-12-10 19:12:32 +0100

    feat: testing script update options

--- testing.sh
+++ testing.sh
@@ -7,6 +7,8 @@ cargo build ; ./target/debug/rusty-configs delete -p /tmp/test
 cargo build ; ./target/debug/rusty-configs delete -i 1
 cargo build ; ./target/debug/rusty-configs delete -n test
 cargo build ; ./target/debug/rusty-configs add -p /tmp/test4 -g 1
+cargo build ; ./target/debug/rusty-configs list version
+cargo build ; ./target/debug/rusty-configs list config
 "
 cmd=$(echo "$commands" | fzf)
 [[ ! -z $cmd ]] && echo -e "=========\nrunning command:\n$cmd\n=========" && bash -c "$cmd"
